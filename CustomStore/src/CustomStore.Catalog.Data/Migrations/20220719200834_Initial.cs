6f94044f472c8340fd9ae54c3a4dce88b9f7bf28
Mateus Junho <mateus.junho@farfetch.com>
2022-08-01 15:32:53 +0100

    feat: add db migration

---
+++ CustomStore/src/CustomStore.Catalog.Data/Migrations/20220719200834_Initial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomStore.Catalog.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Code = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CategoryId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    RegisterDate = table.Column<DateTime>(nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: true),
+                    Width = table.Column<int>(type: "int", nullable: true),
+                    Depth = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ CustomStore/src/CustomStore.WebApp.MVC/Configuration/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using CustomStore.Catalog.Application.Interfaces;
+using CustomStore.Catalog.Application.Services;
+using CustomStore.Catalog.Data.Contexts;
+using CustomStore.Catalog.Data.Repositories;
+using CustomStore.Catalog.Domain.Interfaces.Repository;
+using CustomStore.Catalog.Domain.Interfaces.Services;
+using CustomStore.Catalog.Domain.Services;
+using CustomStore.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomStore.WebApp.MVC.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            // Catalog
+            services.AddScoped<CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<IStockService, StockService>();
+
+            services.AddScoped<IProductAppService, ProductAppService>();
+        }
+    }
+}
